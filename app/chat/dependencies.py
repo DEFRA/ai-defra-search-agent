3c804cadf866412b49622db28f03d28fb5958f0b
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-11-14 10:21:38 +0000

    Inject repository into chat service

--- app/chat/dependencies.py
+++ app/chat/dependencies.py
@@ -1,13 +1,22 @@
 import fastapi
 
-from app.chat import agent, service
+from app.chat import agent, service, repository
 
 
 def get_chat_agent() -> agent.AbstractChatAgent:
     pass
 
 
+def get_conversation_repository() -> repository.AbstractConversationRepository:
+    pass
+
+
 def get_chat_service(
     chat_agent: agent.AbstractChatAgent = fastapi.Depends(get_chat_agent),
+    conversation_repository: repository.AbstractConversationRepository = fastapi.Depends(
+        get_conversation_repository
+    ),
 ) -> service.ChatService:
-    return service.ChatService(chat_agent=chat_agent)
+    return service.ChatService(
+        chat_agent=chat_agent, conversation_repository=conversation_repository
+    )
