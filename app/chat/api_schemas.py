a264b88f5399717bd5946ce26cbb89fef655933d
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-11-18 17:53:08 +0000

    Upgrade pydantic to resolve warnings

--- app/chat/api_schemas.py
+++ app/chat/api_schemas.py
@@ -1,28 +1,31 @@
 import uuid
-from typing import Annotated
 
 import pydantic
+import pydantic.alias_generators
 
 
-class ChatRequest(pydantic.BaseModel):
+class BaseRequestSchema(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(
+        alias_generator=pydantic.alias_generators.to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
+
+class ChatRequest(BaseRequestSchema):
     question: str = pydantic.Field(
         description="The user's question to ask the chat agent",
         min_length=1,
         examples=["What is the weather like today?"],
     )
-    conversation_id: Annotated[
-        uuid.UUID | None,
-        pydantic.Field(
-            default=None,
-            description="The ID of an existing conversation to continue. If not provided, a new conversation will be started.",
-            examples=["3fa85f64-5717-4562-b374-2c963f66afa6"],
-            alias="conversationId",
-        ),
-    ]
+    conversation_id: uuid.UUID | None = pydantic.Field(
+        default=None,
+        description="The ID of an existing conversation to continue. If not provided, a new conversation will be started.",
+        examples=["3fa85f64-5717-4562-b374-2c963f66afa6"],
+    )
     model_name: str = pydantic.Field(
         description="The name of the model to use for generating the response",
         examples=["Claude 3 Haiku"],
-        alias="modelName",
     )
 
 
