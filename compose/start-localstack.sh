0eefa35bf660463fcbcfe5cc422b610392a76861
Stewart Jumbe <stewarjumbe@gmail.com>
2026-01-20 14:40:45 +0000

    chore: add infrastructure for async job queue
    - Add chat-worker service to docker-compose
    - Add boto3 and motor dependencies
    - Configure LocalStack SQS queue initialization
    - Update LocalStack ports to avoid conflicts

---
+++ compose/start-localstack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export AWS_REGION=eu-west-2
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
+# SQS queue for chat jobs
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name chat-job-queue
